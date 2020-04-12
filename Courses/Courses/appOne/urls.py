7e4fc5f2c023408bd28a7a8cecf891f30e51c540
subodhr258 <58080782+subodhr258@users.noreply.github.com>
2020-04-12 16:44:20 +0530

    Update appOne/urls.py

--- Courses/Courses/appOne/urls.py
+++ Courses/Courses/appOne/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from appOne import views
 
 urlpatterns = [
-path('',views.courses,name='courses'),
+path('courses/',views.courses,name='courses'),
+path('aboutus/',views.aboutus,name='aboutus'),
+path('',views.index,name='index'),
+path('contact/',views.contact,name='contact'),
 ]
