e38ea0aee4ea9784826d3fabf0cf64182d4e92de
subodhr258 <58080782+subodhr258@users.noreply.github.com>
2020-04-12 16:45:36 +0530

    Update views.py

--- Courses/Courses/appOne/views.py
+++ Courses/Courses/appOne/views.py
@@ -5,6 +5,12 @@ from appOne.models import Course
 def index(request):
     return render(request,'appOne/index.html')
 
+def aboutus(request):
+    return render(request,'appOne/aboutus.html')
+
+def contact(request):
+    return render(request,'appOne/contact.html')
+
 def courses(request):
     course_list = Course.objects.order_by('course_name')
     course_dict = {'courses':course_list}
